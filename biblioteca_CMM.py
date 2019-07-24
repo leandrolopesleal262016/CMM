5ed04da2be62f0658eb179c722dd6560b5a987e8
leandrolopesleal <leandrolopesleal26@gmail.com>
2019-07-24 08:33:17 -0300

    Esta biblioteca importa todas as bibliotecas do CMM

---
+++ biblioteca_CMM.py
@@ -0,0 +1,6 @@
+from biblioteca_CMM_oficial import Rele,Narrador,Temperatura,Email,Clima,Evento
+from cmm_io import Entradas, Saidas # Classes para leituras das entradas e acionamento das saidas definidas no arq. config.txt
+from expansor_modbus import Expansor # Acionamento de saidas dos expansores
+from leitor_modbus import Leitor  # Leitura das entradas dos expansores
+from IHM_Sociais import IHM
+from banco import Banco
