3ef5e46810fffb5fe734ee5deeaf2d128b49b6a5
Leandro Lopes Leal <leandrolopesleal26@gmail.com>
2019-10-31 11:06:32 -0300

    Rodar 1 vez por dia as 23:59hs
    Programa que salva o log do dia, que esta em "/var/www/html/log/log.txt" na pasta correspondente ao mês

---
+++ gerenciador_registros_log.py
@@ -0,0 +1,128 @@
+import time
+import os
+import glob
+import shutil
+
+# Data atual formatada com"_"
+
+data = time.strftime('%d_%m_%y')
+mes = time.strftime('%m')
+dia = time.strftime('%d')
+
+hs = time.strftime("%H:%M:%S")
+
+##print(data)
+
+if mes == "1":
+    mes = "Janeiro"
+    
+    fazer_backup = 31
+
+if mes == "2":
+    mes = "Fevereiro"
+
+    fazer_backup = 28
+
+if mes == "3":
+    mes = "Marco"
+
+    fazer_backup = 31
+
+if mes == "4":
+    mes = "Abril"
+
+    fazer_backup = 30
+
+if mes == "5":
+    mes = "Maio"
+
+    fazer_backup = 31
+
+if mes == "6":
+    mes = "Junho"
+
+    fazer_backup = 30
+
+if mes == "7":
+    mes = "Julho"
+
+    fazer_backup = 31
+
+if mes == "8":
+    mes = "Agosto"
+
+    fazer_backup = 31
+
+if mes == "9":
+    mes = "Setembro"
+
+    fazer_backup = 30
+
+if mes == "10":
+    mes = "Outubro"
+
+    fazer_backup = 31
+
+if mes == "11":
+    mes = "Novembro"
+
+    fazer_backup = 30
+
+if mes == "12":
+    mes = "Dezembro"
+
+    fazer_backup = 31
+
+# Cria uma lista com os nomes contidos na oasta log em formato .txt
+
+path = '/var/www/html/log'
+folder = os.fsencode(path)
+
+filenames = []
+
+for file in os.listdir(folder):
+    filename = os.fsdecode(file)
+    if filename.endswith( ('.txt')): #, '.png', '.gif') ): # poderia listar outros tipos tambem
+        filenames.append(filename)
+                        
+quantidade = (len(filenames)) # Quantidade de arquivos na pasta
+##print(filenames) # imprime a lista
+
+##print("Temos",quantidade, "arquivos na pasta")
+
+# Limpando a pasta caso ja tenha a quantidade maxima de arquivos
+
+if quantidade >= fazer_backup:
+    
+    print("Limpando a pasta que ja contem o limite de arquivos do mes...")
+
+    diretorio = ("/var/www/html/log/{}").format(mes)
+
+    shutil.rmtree(diretorio)
+    os.mkdir(diretorio)
+    msg = ("A pasta atual de {}, substituiu a pasta do ano passado").format(mes)
+    print(msg) 
+
+# Salvando o log do dia no arquivo e pasta correspondente
+
+fonte = "/var/www/html/log/log.txt"
+destino = ("/var/www/html/log/{}/{}.txt").format(mes,data)
+
+try:        
+
+    shutil.copyfile(fonte,destino) # Tenta salvar o log.txt como um novo arquivo 
+
+except : # Se for a primeira vez e nao existir pasta, cria uma.
+    
+    print("Criando a pasta do mes que ainda nao existia...")
+    diretorio = ("/var/www/html/log/{}").format(mes)
+    os.mkdir(diretorio)
+    
+    shutil.copyfile(fonte,destino)
+
+
+print("Salvo o log do dia na pasta correspondente")    
+
+
+
+    
